436226acc242c18271ab87cf7f89b89a72307151
Ron Segal <4ronse@gmail.com>
2023-04-12 14:55:11 +0300

    Made some changes
    Added some static colors. Did some changes made things simpler....

--- src/main/java/org/ronse/autoupnp/AutoUPnP.java
+++ src/main/java/org/ronse/autoupnp/AutoUPnP.java
@@ -1,9 +1,7 @@
 package org.ronse.autoupnp;
 
 import com.dosse.upnp.UPnP;
-import it.unimi.dsi.fastutil.Pair;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,61 +9,62 @@ import java.util.ArrayList;
 
 public final class AutoUPnP extends JavaPlugin {
     public static AutoUPnP instance;
-    public final ArrayList<Pair<Integer, Boolean>> ports;
+    public static ConfigHelper configHelper;
 
-    // Static messages, don't see why they should be inlined
-    static final TextComponent SEARCHING;
-    static final TextComponent DONE_SEARCH;
+    public static final int COLOR_INFO      = 0x0dcaf0;
+    public static final int COLOR_WARN      = 0xfecf3e;
+    public static final int COLOR_DANGER    = 0xdc3545;
 
-    static {
-        SEARCHING = Component.text("Searching for UPnP enabled device").color(TextColor.color(0x00A680));
-        DONE_SEARCH = Component.text("UPnP Gateway found!").color(TextColor.color(0x00A680));
-    }
+    public static final ArrayList<ConfigHelper.Port> ports = new ArrayList<>();;
 
     public AutoUPnP() {
         instance = this;
-        ports = new ArrayList<>();
+        configHelper = new ConfigHelper();
     }
 
     @Override
     public void onEnable() {
-        getComponentLogger().info(SEARCHING);
-        UPnP.waitInit();
-        getComponentLogger().info(DONE_SEARCH);
+        openPorts();
+    }
 
+    public void openPorts() {
+        configHelper.reload();
+        configHelper.config.ports.forEach(port -> {
+            try {
+                getComponentLogger().info(Component.text("Trying to open port " + port.toString()).
+                        color(TextColor.color(COLOR_INFO)));
+                openPort(port);
+                getComponentLogger().info(Component.text(port + " is open").
+                        color(TextColor.color(COLOR_INFO)));
+            } catch (RuntimeException ex) {
+                getComponentLogger().error(Component.text("Failed to open port " + port.toString() +
+                        "\n" + ex).color(TextColor.color(COLOR_DANGER)));
+            }
+        });
+    }
 
-        ConfigHelper helper = new ConfigHelper();
-        helper.config.ports.forEach(port -> {
-            UPnP.defaultGW.openPort(port.ip(), port.internalPort(), port.externalPort(), port.description(),
-                    port.protocol() == Protocol.UDP
-            );
-
-            getComponentLogger().info(Component.text("%s:%d forwarded".formatted(port.ip(), port.internalPort()))
-                    .color(TextColor.color(0x008030)));
-
-            ports.add(new Pair<>() {
-                @Override
-                public Integer left() {
-                    return port.externalPort();
-                }
-
-                @Override
-                public Boolean right() {
-                    return port.protocol() == Protocol.UDP;
-                }
-
-            });
-
+    public void openPort(ConfigHelper.Port port) throws RuntimeException {
+        if(port.disabled()) {
+            getComponentLogger().info(Component.text(port + " is disabled")
+                    .color(TextColor.color(COLOR_WARN)));
+            return;
         }
-        );
+
+        UPnP.waitInit();
+        if(!UPnP.defaultGW.openPort(port.ip(), port.internalPort(), port.externalPort(), port.description(),
+                port.protocol() == Protocol.UDP)) throw new RuntimeException("Failed to open port!");
+
+        ports.add(port);
     }
 
     @Override
     public void onDisable() {
-        this.ports.forEach(pair -> {
-            UPnP.defaultGW.closePort(pair.left(), pair.right());
-            getComponentLogger().info(Component.text("%d disabled!".formatted(pair.left()))
-                    .color(TextColor.color(0xA00000)));
+        ports.forEach(port -> {
+            UPnP.defaultGW.closePort(port.externalPort(), port.protocol() == Protocol.UDP);
+            getComponentLogger().info(Component.text(port + " closed")
+                    .color(TextColor.color(COLOR_WARN)));
         });
+
+        ports.clear();
     }
 }
--- src/main/java/org/ronse/autoupnp/ConfigHelper.java
+++ src/main/java/org/ronse/autoupnp/ConfigHelper.java
@@ -3,6 +3,8 @@ package org.ronse.autoupnp;
 import de.exlll.configlib.Comment;
 import de.exlll.configlib.Configuration;
 import de.exlll.configlib.YamlConfigurations;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -15,23 +17,28 @@ public final class ConfigHelper {
                        @Comment("Integer between 0-65535") int internalPort,
                        @Comment("Integer between 0-65535") int externalPort,
                        @Comment("Either TCP or UDP") Protocol protocol,
-                       @Comment("Service description such as \"Minecraft Server\"") String description
+                       @Comment("Service description such as \"Minecraft Server\"") String description,
+                       boolean disabled
     ) {}
 
     @Configuration
     public static class BaseConfiguration {
-        public List<Port> ports = List.of(new Port("0.0.0.0", 25565, 25565, Protocol.TCP, "Minecraft Server"));
+        public List<Port> ports = List.of(new Port("0.0.0.0", 25565, 25565, Protocol.TCP, "Minecraft Server", false));
     }
 
     public BaseConfiguration config;
     private final File file = new File(AutoUPnP.instance.getDataFolder(), "ports.yml");
 
     public ConfigHelper() {
-
         if(!file.exists()) YamlConfigurations.save(file.getAbsoluteFile().toPath(), BaseConfiguration.class,
                 new BaseConfiguration());
 
+        reload();
+    }
+
+    public void reload() {
         config = YamlConfigurations.load(file.getAbsoluteFile().toPath(), BaseConfiguration.class);
+        AutoUPnP.instance.getComponentLogger().info(Component.text("ports.yml Reloaded").color(TextColor.color(AutoUPnP.COLOR_INFO)));
     }
 
     public void update() {
